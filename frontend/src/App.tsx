e025a8dad02f94e2b179d08051f198d16d97128c
Massimo Costa <costa.massimo@gmail.com>
2023-01-11 19:53:08 +0100

    initial UI skeleton

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,48 @@
-import {useState} from 'react';
-import logo from './assets/images/logo-universal.png';
-import './App.css';
-import {Greet} from "../wailsjs/go/main/App";
-
 function App() {
-    const [resultText, setResultText] = useState("Please enter your name below 👇");
-    const [name, setName] = useState('');
-    const updateName = (e: any) => setName(e.target.value);
-    const updateResultText = (result: string) => setResultText(result);
-
-    function greet() {
-        Greet(name).then(updateResultText);
-    }
-
-    return (
-        <div id="App">
-            <img src={logo} id="logo" alt="logo"/>
-            <div id="result" className="result">{resultText}</div>
-            <div id="input" className="input-box">
-                <input id="name" className="input" onChange={updateName} autoComplete="off" name="input" type="text"/>
-                <button className="btn" onClick={greet}>Greet</button>
-            </div>
+  return (
+    <div>
+      <div className="navbar bg-base-200">
+        <div className="flex-none">
+          <label
+            htmlFor="my-drawer-2"
+            className="btn btn-square btn-ghost lg:hidden"
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              className="inline-block w-5 h-5 stroke-current"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16M4 18h16"
+              ></path>
+            </svg>
+          </label>
         </div>
-    )
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-xl">Matematica</a>
+        </div>
+      </div>
+      <div className="drawer drawer-mobile">
+        <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+        <div className="drawer-content flex flex-col items-center justify-center"></div>
+        <div className="drawer-side">
+          <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+          <ul className="menu p-4 w-80 bg-base-100 text-base-content">
+            <li>
+              <a>Moltiplicazioni</a>
+            </li>
+            <li>
+              <a>Divisioni</a>
+            </li>
+          </ul>
+        </div>
+      </div>{" "}
+    </div>
+  );
 }
 
-export default App
+export default App;
